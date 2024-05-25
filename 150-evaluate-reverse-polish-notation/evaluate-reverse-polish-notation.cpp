e9fbb59e3404a9493f3216a91f507f21e1efd635
Yousef Mahmoud <129234471+usefmahmud@users.noreply.github.com>
2024-05-25 05:40:55 +0300

    Time: 8 ms (60.53%) | Memory: 15.4 MB (55.79%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    void evaluate(stack<int>& s, string& oper){
-        int x = s.top();
-        s.pop();
-        int y = s.top();
-        s.pop();
-        if(oper == "+"){
-            s.push(x + y);
-        }else if(oper == "*"){
-            s.push(x * y);
-        }else if(oper == "-"){
-            s.push(y - x);
-        }else if(oper == "/"){
-            s.push(y / x);
-        }
-    }
 int evalRPN(vector<string>& tokens) {
     stack<int> stk;
     for(auto& x: tokens){
         if(stk.empty()) stk.push(stoi(x));
         else{
             if(x == "+" || x == "*" || x == "/" || x == "-"){
-                evaluate(stk,x);
+                int a = stk.top(); stk.pop();
+                int b = stk.top(); stk.pop();
+                if(x == "+"){
+                    stk.push(a + b);
+                }else if(x == "*"){
+                    stk.push(a * b);
+                }else if(x == "-"){
+                    stk.push(b - a);
+                }else if(x == "/"){
+                    stk.push(b / a);
+                }
             }else{
                 stk.push(stoi(x));
             }
