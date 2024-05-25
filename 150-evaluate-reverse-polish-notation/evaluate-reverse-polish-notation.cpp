4d98937ffb777c98d2bfa7a85feacb0cede81795
Yousef Mahmoud <129234471+usefmahmud@users.noreply.github.com>
2024-05-25 05:38:34 +0300

    Time: 4 ms (90.13%) | Memory: 16.6 MB (5.21%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
     void evaluate(stack<int>& s, string& oper){
-    if(oper == "+"){
         int x = s.top();
         s.pop();
         int y = s.top();
         s.pop();
-        s.push(x + y);
+        if(oper == "+"){
+            s.push(x + y);
+        }else if(oper == "*"){
+            s.push(x * y);
+        }else if(oper == "-"){
+            s.push(y - x);
+        }else if(oper == "/"){
+            s.push(y / x);
+        }
     }
-    if(oper == "*"){
-        int x = s.top();
-        s.pop();
-        int y = s.top();
-        s.pop();
-        s.push(x * y);
-    }
-    if(oper == "-"){
-        int x = s.top();
-        s.pop();
-        int y = s.top();
-        s.pop();
-        s.push(y - x);
-    }
-    if(oper == "/"){
-        int x = s.top();
-        s.pop();
-        int y = s.top();
-        s.pop();
-        s.push(y / x);
-    }
-}
 int evalRPN(vector<string>& tokens) {
     stack<int> stk;
     for(auto& x: tokens){
