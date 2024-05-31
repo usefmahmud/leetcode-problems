8733d3ac7fd9ccfb4234c848f29dda2e8e624e57
Yousef Mahmoud <129234471+usefmahmud@users.noreply.github.com>
2024-05-31 08:51:58 +0300

    Time: 125 ms (19.20%) | Memory: 109.9 MB (53.49%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    long long zeroFilledSubarray(vector<int>& nums) {
+    long long otherSoln(vector<int>& nums){
         long long r=0,c=0;
         for(int i=0; i<nums.size(); i++){
             if(nums[i] == 0){
@@ -11,6 +11,24 @@ public:
             }
         }
         r += c*(c+1)/2;
+        return r;
+    }
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long r=0, i=0;
+        while(i < nums.size()){
+            if(nums[i] == 0){
+                int j=i;
+                long long c=0;
+                while(j < nums.size() && nums[j] == 0){
+                    c++;
+                    j++;
+                }
+                r += c*(c+1)/2;
+
+                i=j;
+            }
+            i++;
+        }
         return r;
     }
 };
