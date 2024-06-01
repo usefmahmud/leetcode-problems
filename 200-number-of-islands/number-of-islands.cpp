e24ca2ef746d8ae6736f5c2b5dc000e1029a4c0d
Yousef Mahmoud <129234471+usefmahmud@users.noreply.github.com>
2024-06-01 11:30:12 +0300

    Time: 25 ms (76.61%) | Memory: 15.9 MB (56.87%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    void visit(vector<vector<char>>& grid, int row, int col){
-        grid[row][col] = '0';
-        
-        // condition to break if there is no '1'
-        if(row > 0 && grid[row-1][col] != '0'){
-            visit(grid, row-1, col);
+    void visit(vector<vector<char>>& grid, int r, int c, int m, int n){
+        if (r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == '0') {
+            return;
         }
-        if(col > 0 && grid[row][col-1] != '0'){
-            visit(grid, row, col-1);
-        }
-        if(row < grid.size()-1 && grid[row+1][col] != '0'){
-            visit(grid, row+1, col);
-        }
-        if(col < grid[0].size()-1 && grid[row][col+1] != '0'){
-            visit(grid, row, col+1);
-        }
+        grid[r][c] = '0';
+
+        visit(grid, r-1, c, m, n);
+        visit(grid, r+1, c, m, n);
+        visit(grid, r, c-1, m, n);
+        visit(grid, r, c+1, m, n);
     }
     int numIslands(vector<vector<char>>& grid) {
         int r=0;
+        int m=grid.size(), n=grid[0].size();
         for(int i=0; i<grid.size(); i++){
             for(int j=0; j<grid[0].size(); j++){
                 if(grid[i][j] == '1'){
                     r++;
-                    visit(grid, i, j);
+                    visit(grid, i, j, m, n);
                 }
             }
         }
