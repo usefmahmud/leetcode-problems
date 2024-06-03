362defc5405009d88ab4e94bc078936532d4b93b
Yousef Mahmoud <129234471+usefmahmud@users.noreply.github.com>
2024-06-03 08:05:01 +0300

    Time: 23 ms (20.38%) | Memory: 12 MB (11.40%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
@@ -4,10 +4,9 @@ public:
         int i=0, j=0;
         while(i < s.length() && j < t.length()){
             if(s[i] == t[j]){
-                i++, j++;
-            }else{
-                i++;
+                j++;
             }
+            i++;
         }
 
         return t.length() - j;
