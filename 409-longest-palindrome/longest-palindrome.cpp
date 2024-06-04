129ce5d902296697d41c821b2be9da598783bf20
Yousef Mahmoud <129234471+usefmahmud@users.noreply.github.com>
2024-06-04 04:43:39 +0300

    Time: 2 ms (63.38%) | Memory: 9.1 MB (6.93%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        int freq1[26]={0};
-        int freq2[26]={0};
-        for(auto& c: s){
-            if(c >= 'a') ++freq1[c-'a'];
-            else ++freq2[c-'A'];
-        }
+        unordered_map<char, int> m;
+        for(auto& c: s) ++m[c];
         int odd = 0, r = 0;
-        for(int i=0; i<26; i++){
-            r += freq1[i]-(freq1[i]%2);
-            if(freq1[i]%2) odd=1;
-            
-            r += freq2[i]-(freq2[i]%2);
-            if(freq2[i]%2) odd=1;
+        for(auto& x: m){
+            if(x.second % 2 == 0){
+                r += x.second;
+            }else{
+                r += x.second-1;
+                odd = 1;
+            }
         }
         
         return r + odd;
