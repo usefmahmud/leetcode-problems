eec45bf2fed50dbc1b9f1d213b536e2365063e9b
Yousef Mahmoud <129234471+usefmahmud@users.noreply.github.com>
2024-06-05 04:22:13 +0300

    Time: 2 ms (96.39%) | Memory: 11.1 MB (82.02%) - LeetSync

--- 1044-find-common-characters/find-common-characters.cpp
+++ 1044-find-common-characters/find-common-characters.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     vector<string> commonChars(vector<string>& words) {
-        unordered_map<char, int> m;
+        vector<int> f(26, INT_MAX);
         vector<string> r;
         for(int i=0; i<words.size(); i++){
             int freq[26]{0};
             for(auto& c: words[i]){
                 freq[c - 'a']++;
             }
-            if(i == 0){
-                for(int j=0; j<26; j++){
-                    m[char('a' + j)] = freq[j];
-                }
-            }else{
-                for(int j=0; j<26; j++){
-                    m[char('a' + j)] = min(freq[j], m[char('a' + j)]);
-                }
+            for(int j=0; j<26; j++){
+                f[j] = min(freq[j], f[j]);
             }
         }
-        for(auto& x: m){
-            for(int i=0; i<x.second; i++){
-                string g = "";
-                g+=char(x.first);
-                r.push_back(g);
+        for(int i=0; i<26; i++){
+            for(int j=0; j<f[i]; j++){
+                r.push_back(string(1, 'a' + i));
             }
         }
         return r;
