4af9bebc02593fe40224d028dd929c539e45e760
Yousef Mahmoud <129234471+usefmahmud@users.noreply.github.com>
2024-06-16 21:27:54 +0300

    Time: 0 ms (100.00%) | Memory: 11.3 MB (11.93%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -5,11 +5,18 @@ public:
         if(n == 1 || nums[0] > nums[1]) return 0;
         if(nums[n-1] > nums[n-2]) return n-1;
 
-        for(int i=2; i<nums.size(); i++){
-            if(nums[i-1] > nums[i] && nums[i-1] > nums[i-2]){
-                return i-1;
+        int l = 1, r = n - 2;
+        while(l <= r){
+            int m = (r + l) / 2;
+            if(nums[m] > nums[m+1] && nums[m] > nums[m-1]){
+                return m;
+            }else if(nums[m-1] > nums[m]){
+                r = m-1;
+            }else if(nums[m+1] > nums[m]){
+                l = m+1;
             }
         }
+
 
         return 0;
     }
