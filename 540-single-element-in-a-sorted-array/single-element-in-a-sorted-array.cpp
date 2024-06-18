21c73472997060a48c6a0fa2483981a760cd32a6
Yousef Mahmoud <129234471+usefmahmud@users.noreply.github.com>
2024-06-18 19:31:43 +0300

    Time: 17 ms (73.09%) | Memory: 24.7 MB (51.40%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -5,24 +5,13 @@ public:
         if(r == 0) return nums[0];
         while(l < r){
             m = (l + r) / 2;
-            if(m > 0 && m < nums.size()-1 && nums[m] != nums[m+1] && nums[m] != nums[m-1]){
-                return nums[m];
-            }else if(m == 0 && nums[m] != nums[m+1]){
-                return nums[m];
-            }else if(m == nums.size()-1 && nums[m] != nums[m-1]){
-                return nums[m];
-            }else if(m > 0 && nums[m] == nums[m-1]){
-                if(m%2 != 0){
-                    l = m + 1;
-                }else{
-                    r = m - 1;
-                }
-            }else if(m < nums.size()-1 && nums[m] == nums[m+1]){
-                if(m%2 == 0){
-                    l = m + 1;
-                }else{
-                    r = m - 1;
-                }
+            
+            if(m%2 != 0) --m;
+            
+            if (nums[m] == nums[m+1]) {
+                l = m + 2;
+            } else {
+                r = m;
             }
         }
 
