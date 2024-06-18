990558c2db9fa0a38941be4ff87c60a4fc10d2de
Yousef Mahmoud <129234471+usefmahmud@users.noreply.github.com>
2024-06-18 05:32:55 +0300

    Time: 3 ms (64.41%) | Memory: 9 MB (13.09%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -2,14 +2,9 @@ class Solution {
 public:
     int maxNumberOfBalloons(string text) {
         unordered_map<char,int> m;
-        string word = "balloon";
         int r = 0;
         for(char c: text) ++m[c];
-        while(m['b'] && m['a'] && m['l']>1 && m['o']>1 && m['n']){
-            r++;
-            for(char c: word) --m[c];
-        }
 
-        return r;
+        return min({m['b'], m['a'], m['n'], m['o']/2, m['l']/2});
     }
 };
