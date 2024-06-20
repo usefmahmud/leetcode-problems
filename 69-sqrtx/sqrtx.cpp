3175b1f462051351311660acabe34b55bd15005b
Yousef Mahmoud <129234471+usefmahmud@users.noreply.github.com>
2024-06-21 01:44:12 +0300

    Time: 2 ms (50.94%) | Memory: 7.4 MB (80.55%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        long long l = 1, r = x, m;
+        int res = 0;
+        while(l <= r){
+            m = (l + r) / 2;
+            if(m * m <= x){
+                res = m;
+                l = m + 1;
+            }else{
+                r = m - 1;
+            }
+        }
+
+        return res;
     }
 };
