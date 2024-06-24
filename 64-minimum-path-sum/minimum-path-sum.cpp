674d4bf6787ace11c5908de5916f251ac93c1c45
Yousef Mahmoud <129234471+usefmahmud@users.noreply.github.com>
2024-06-24 04:58:40 +0300

    Time: 11 ms (18.50%) | Memory: 12.6 MB (58.79%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int getMin(vector<vector<int>>& g, int i, int j, vector<vector<int>>& dp){
-        if(i == 0 && j == 0){
+    int getMin(vector<vector<int>>& g, int i, int j, vector<vector<int>>& dp, int r, int c){
+        if(i > r-1 || j > c-1){
+            return INT_MAX;
+        }
+        if(i == r-1 && j == c-1){
             return g[i][j];
         }
         if(dp[i][j] > -1){
            return dp[i][j];
         }
-        dp[i][j] = INT_MAX;
-        if(i > 0){
-            dp[i][j] = min(dp[i][j], g[i][j] + getMin(g,i-1,j,dp));
-        }
-        if(j > 0){
-            dp[i][j] = min(dp[i][j], g[i][j] + getMin(g,i,j-1,dp));
-        }
+        dp[i][j] = g[i][j] + min(getMin(g,i+1,j,dp,r,c), getMin(g,i,j+1,dp,r,c));
+        
         return dp[i][j];
     }
     int minPathSum(vector<vector<int>>& g) {
         int r = g.size(), c = g[0].size();
         vector<vector<int>> memo(r,vector<int>(c, -1));
-        return getMin(g, r-1, c-1, memo);
+        return getMin(g, 0, 0, memo, g.size(), g[0].size());
     }
 };
