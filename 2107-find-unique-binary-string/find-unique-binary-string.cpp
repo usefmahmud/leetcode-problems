3c1a6311738aedb3f2cc3591707c13937cedff94
Yousef Mahmoud <129234471+usefmahmud@users.noreply.github.com>
2024-06-25 03:57:24 +0300

    Time: 0 ms (100.00%) | Memory: 11.9 MB (85.83%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,21 +1,11 @@
 class Solution {
-    
 public:
-    unordered_map<string, int> m;
-    string r;
-    void generate(int n, string g){
-        if(n == 0){
-            if(m.find(g) == m.end()){
-                r = g;
-            }
-        }else{
-            generate(n-1, g + "1");
-            generate(n-1, g + "0");
-        }
-    }
     string findDifferentBinaryString(vector<string>& nums) {
-        for(auto x: nums) ++m[x];
-        generate(nums[0].size(), "");
+        string r = "";
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i][i] == '1') r += '0';
+            else r += '1';
+        }
         return r;
     }
 };
