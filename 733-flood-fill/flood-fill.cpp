53de8350ee4218a5e619cca04fd13c8adb39aa69
Yousef Mahmoud <129234471+usefmahmud@users.noreply.github.com>
2024-06-29 09:54:51 +0300

    Time: 4 ms (83.74%) | Memory: 16.6 MB (84.37%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     void go(int i, int j, int color, int target, vector<vector<int>>& g){
-        if(i < 0 || j < 0 || i >= g.size() || j >= g[0].size()){
-            return;
-        }
-        if(g[i][j] != target){
-            return;
-        }
-        if(g[i][j] == color){
-            return;
-        }
+        if(i < 0 || j < 0 || i >= g.size() || j >= g[0].size()) return;
+        if(g[i][j] == color || g[i][j] != target) return;
 
         g[i][j] = color;
-
         go(i+1, j, color, target, g);
         go(i-1, j, color, target, g);
         go(i, j+1, color, target, g);
