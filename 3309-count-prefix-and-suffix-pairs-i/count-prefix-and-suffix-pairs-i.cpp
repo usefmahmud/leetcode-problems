dcede521c3dedf28e0f11171227906c19387ac17
Yousef Mahmoud <129234471+usefmahmud@users.noreply.github.com>
2024-07-12 02:10:13 +0300

    Time: 7 ms (81.24%) | Memory: 24.1 MB (10.63%) - LeetSync

---
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    bool isPrefix(string a, string b){
+        int i = 0, j = 0, n = a.length(), m = b.length();
+        while(i < n && j < m){
+            if(a[i] == b[j]){
+                i++, j++;
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+
+    bool isSuffix(string a, string b){
+        int i = a.length() - 1, j = b.length() - 1;
+        while(i >= 0 && j >= 0){
+            if(a[i] == b[j]){
+                i--, j--;
+                continue;
+            }
+            return false;
+        }
+        return i == -1;
+    }
+public:
+    int countPrefixSuffixPairs(vector<string>& words) {
+        int r = 0;
+        for(int i = 0; i < words.size(); i++){
+            for(int j = i + 1; j < words.size(); j++){
+                if(isPrefix(words[i], words[j]) && isSuffix(words[i], words[j])){
+                    r++;
+                }
+            }
+        }
+        return r;
+    }
+};
