acb4b782980dbdfd886b0f0fb59c0f7e21299713
Yousef Mahmoud <129234471+usefmahmud@users.noreply.github.com>
2024-07-11 08:27:41 +0300

    Time: 45 ms (18.82%) | Memory: 28 MB (6.13%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
@@ -2,7 +2,10 @@ class Solution {
 public:
     int numEquivDominoPairs(vector<vector<int>>& d) {
         map<pair<int,int>, int> m;
-        for(auto x: d) ++m[make_pair(min(x[0], x[1]), max(x[0], x[1]))];
+        for(auto x: d){
+            if(x[1] > x[0]) swap(x[0], x[1]);
+            ++m[{x[0], x[1]}];
+        }
         int r = 0;
         for(auto x: m){
             if(x.second > 1){
