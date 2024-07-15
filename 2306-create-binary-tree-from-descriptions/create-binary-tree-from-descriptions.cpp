a2461eacdc60b2486c3efc046ebdee86cf168684
Yousef Mahmoud <129234471+usefmahmud@users.noreply.github.com>
2024-07-15 05:11:47 +0300

    Time: 662 ms (81.44%) | Memory: 276.2 MB (59.61%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -10,44 +10,35 @@
  * };
  */
 class Solution {
-    unordered_map<int, pair<int,int>> m;
 public:
-    void go(TreeNode* b, int val, bool isLeft){
-        if(val == 0) return;
-        TreeNode* x = new TreeNode(val);
-        if(isLeft) b->left = x;
-        else b->right = x;
-
-        go(x, m[val].first, true);
-        go(x, m[val].second, false);
-    }
-
-    int getHead(unordered_set<int>& a, unordered_set<int>& b){
-        for(auto x: a){
-            if(b.find(x) == b.end()){
-                return x;
-            }
-        }
-        return 0;
-    }
 
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        unordered_set<int> a,b;
-        for(auto x: descriptions){
-            a.insert(x[0]), a.insert(x[1]);
-            b.insert(x[1]);
-            if(x[2]){
-                m[x[0]].first = x[1];
+        unordered_map<int, TreeNode*> m;
+        unordered_set<int> st;
+        for (auto& x: descriptions) {
+            int parent = x[0], child = x[1], isLeft = x[2];
+
+            if(!m.count(parent)){
+                m[parent] = new TreeNode(parent);
+            }
+            if(!m.count(child)){
+                m[child] = new TreeNode(child);
+            }
+
+            if (isLeft){
+                m[parent]->left = m[child];
             }else{
-                m[x[0]].second = x[1];
+                m[parent]->right = m[child];
+            }
+
+            st.insert(child);
+        }
+        for(auto& x: m){
+            if(!st.count(x.first)){
+                return x.second;
             }
         }
-        int h = getHead(a,b);
 
-        TreeNode* root = new TreeNode(h);
-        go(root, m[h].first, true);
-        go(root, m[h].second, false);
-
-        return root;
+        return NULL;
     }
 };
