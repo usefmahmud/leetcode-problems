84b917b541a52c00aa403745b53d7b2b5eed6119
Yousef Mahmoud <129234471+usefmahmud@users.noreply.github.com>
2024-07-18 14:45:07 +0300

    Time: 50 ms (49.68%) | Memory: 33.6 MB (64.44%) - LeetSync

---
+++ 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> go(TreeNode* b, int& dis, int& r){
+        if(!b) return {};
+        if(!b->right && !b->left) return {1};
+
+        vector<int> left = go(b->left, dis, r);
+        vector<int> right = go(b->right, dis, r);
+        vector<int> tmp;
+
+        for(auto y: left){
+            for(auto x: right){
+                if(x + y <= dis){
+                    r++;
+                }
+            }
+        }
+
+        for(auto y: left){
+            if(y < dis){
+                tmp.push_back(y + 1);
+            }
+        }
+
+        for(auto x: right){
+            if(x < dis){
+                tmp.push_back(x + 1);
+            }
+        }
+
+        return tmp;
+    }
+    int countPairs(TreeNode* root, int distance) {
+        int r = 0;
+        go(root, distance, r);
+
+        return r;
+    }
+};
